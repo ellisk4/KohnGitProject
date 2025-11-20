b9a953b088ce18bde769e74fa352f574888a97e9
1N3 Computer <sample@bths.edu>
2025-11-20 10:11:41 -0500

    added variables & operators

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello gurt");
+        int x = 5;
+        int y = 10;
+        System.out.println("x + y = " + (x + y) );
+        System.out.println("x - y = " + (x - y) );
+        System.out.println("x * y = " + (x * y) );
+        System.out.println("x / y = " + ((double) x / y) );
     }
 }
