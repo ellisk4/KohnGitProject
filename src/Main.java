86f56759110d320f12b461a92f3bf00bd1a76331
1N3 Computer <sample@bths.edu>
2025-11-20 10:13:33 -0500

    added "z" variable and if statement

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,13 @@ public class Main {
         System.out.println("Hello gurt");
         int x = 5;
         int y = 10;
-        System.out.println("x + y = " + (x + y) );
+        int z = 15;
+        System.out.println("x + y + z = " + (x + y + z) );
         System.out.println("x - y = " + (x - y) );
         System.out.println("x * y = " + (x * y) );
         System.out.println("x / y = " + ((double) x / y) );
+        if (z >= x + y){
+            System.out.println("WOW!");
+        }
     }
 }
