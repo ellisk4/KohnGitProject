357db7a685701d2cb606baa7aa05892952ac1853
1N3 Computer <sample@bths.edu>
2025-11-24 09:45:35 -0500

    added raining variable to FunTimes

---
+++ src/TestRunner.java
@@ -0,0 +1,9 @@
+public class TestRunner {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter >= 0) {
+            counter++;
+        }
+        System.out.println("final counter: " + counter);
+    }
+}
